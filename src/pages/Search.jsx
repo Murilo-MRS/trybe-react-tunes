55fde70c4b90aa78c47552a3608e0e4b601b4561
Murilo-MRS <murilosantana1993@gmail.com>
2022-08-30 15:39:48 -0300

    req 1 search

---
+++ src/pages/Search.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div data-testid="page-search" className="search-container" />
+    );
+  }
+}
+
+export default Search;
