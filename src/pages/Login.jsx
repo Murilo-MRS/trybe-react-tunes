515a0916c06f2a5ba65b7604d817073552220de9
Murilo-MRS <murilosantana1993@gmail.com>
2022-08-30 15:29:02 -0300

    [Murilo Rodrigues Santana] - Trybetunes

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div data-testid="page-login" className="login-container" />
+    );
+  }
+}
+
+export default Login;
