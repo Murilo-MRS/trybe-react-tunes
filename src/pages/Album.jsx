4c7b521b5bf7951088cb91ad429024c07bc25c3e
Murilo-MRS <murilosantana1993@gmail.com>
2022-08-30 15:41:37 -0300

    req 1 album

---
+++ src/pages/Album.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Album extends Component {
+  render() {
+    return (
+      <div data-testid="page-album" className="album-container" />
+    );
+  }
+}
+
+export default Album;
