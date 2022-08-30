be6f72cf40b155493e891df3740777e0a31f748e
Murilo-MRS <murilosantana1993@gmail.com>
2022-08-30 15:43:05 -0300

    req 1 favorites

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Favorites extends Component {
+  render() {
+    return (
+      <div data-testid="page-favorites" className="favorites-container" />
+    );
+  }
+}
+
+export default Favorites;
