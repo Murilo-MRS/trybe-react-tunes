620c1d2d8b9c50981ef94bb7c67d5c76cdd38429
Murilo-MRS <murilosantana1993@gmail.com>
2022-08-31 12:05:40 -0300

    req 2 final corrigido

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
@@ -13,20 +13,20 @@ class App extends Component {
     return (
       <div className="app-container">
         <p>TrybeTunes</p>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={ Login } />
-            <Route exact path="/search" component={ Search } />
-            <Route exact path="/album/:id" component={ Album } />
-            <Route exact path="/favorites" component={ Favorites } />
-            <Route exact path="/profile" component={ Profile } />
-            <Route exact path="/profile/edit" component={ ProfileEdit } />
-            <Route component={ NotFound } />
-          </Switch>
-        </BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route component={ NotFound } />
+        </Switch>
       </div>
     );
   }
 }
 
 export default App;
+// EM ROUTE CASO PRECISE PEGAR OUTRAS PROPS (HISTORY, MATCH... OU FUNCOES CRIADAS NO PAI)
+// USAMOS RENDER(PROPS) =>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,48 +1,73 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../components/Loading';
+// import { Redirect } from 'react-router-dom';
 import { createUser } from '../services/userAPI';
 
 class Login extends Component {
   state = {
     userName: '',
+    isDisabled: true,
+    loading: false,
+    userCreated: false,
   };
 
   handleInputUser = ({ target }) => {
     console.log(target.value);
-    this.setState({ userName: target.value });
+    this.setState({ userName: target.value }, () => {
+      const { userName } = this.state;
+      const userLength = 2;
+      if (userName.length <= userLength) return this.setState({ isDisabled: true });
+      this.setState({ isDisabled: false });
+    });
   };
 
   handleLogin = async (userName) => {
-    await createUser({ name: userName });
-    console.log(await typeof createUser({ name: userName }));
+    // const { history } = this.props;
+    this.setState({ loading: true }, async () => {
+      const promiseCreate = await createUser({ name: userName });
+      if (promiseCreate === 'OK') this.setState({ userCreated: true });
+      this.setState({ loading: false });
+    });
   };
 
   render() {
-    const userLength = 2;
-    const { userName } = this.state;
+    const { userName, isDisabled, loading, userCreated } = this.state;
     return (
-      <div data-testid="page-login" className="login-container">
-        <div className="user-input">
-          <label htmlFor="userName">
-            Nome:
-            {' '}
-            <input
-              type="text"
-              name="userName"
-              value={ userName }
-              data-testid="login-name-input"
-              onChange={ this.handleInputUser }
-            />
-          </label>
-        </div>
-        <button
-          type="button"
-          data-testid="login-submit-button"
-          className="btn"
-          onClick={ () => this.handleLogin(userName) } // ANOTACAO ERRO 1
-          disabled={ userName.length <= userLength }
-        >
-          Entrar
-        </button>
+      <div>
+        {
+          loading
+            ? <Loading />
+            : (
+              <div data-testid="page-login" className="login-container">
+                <div className="user-input">
+                  <label htmlFor="userName">
+                    Nome:
+                    {' '}
+                    <input
+                      type="text"
+                      name="userName"
+                      value={ userName }
+                      data-testid="login-name-input"
+                      onChange={ this.handleInputUser }
+                    />
+                  </label>
+                </div>
+                <button
+                  type="button"
+                  data-testid="login-submit-button"
+                  className="btn"
+                  onClick={ () => this.handleLogin(userName) } // ANOTACAO ERRO 1
+                  disabled={ isDisabled }
+                >
+                  Entrar
+                </button>
+              </div>
+            )
+        }
+        {
+          (userCreated) && <Redirect to="/search" />
+        }
       </div>
     );
   }
@@ -50,3 +75,7 @@ class Login extends Component {
 
 export default Login;
 // ANOTACAO ERRO 1 - SEMPRE UTLIZAR CALLBACK QUANDO PRECISAR PASSAR UMA PARAM EM ONCLICK
+// ANOTACAO ERRO 2 - SO CRIE STATE ONDE FOR USAR PARA NAO AUMENTAR COMPLEXIDADE,(nesse caso no filho de App)
+// ANOTACAO ERRO 3 - handleChange generico necessita do atributo NAME ser o mesmo valor no state
+// ANOTACAO ERRO 4 - segundo parametro de setState para verificar apos atualizacao de estado
+// ANOTACAO ERRO 5 - o PROPS DE ROUTE TEMOS HISTORY, LOCATION E MATCH caso precisamos navegar(redirect) para outra página usamos a prop history q possui a key PUSH
