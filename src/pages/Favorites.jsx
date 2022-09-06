a48b68b56274bd5bf0c1f24f7347d810d9f4df21
Murilo-MRS <murilosantana1993@gmail.com>
2022-09-06 16:07:38 -0300

    req 12 teste 1 e 2 passed

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import Header from '../components/Hearder';
-// import { addSong, removeSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
+  state = {
+    favorites: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const favoriteMusic = await getFavoriteSongs();
+    this.setState({ favorites: favoriteMusic }, () => {
+      if (favoriteMusic) this.setState({ favorites: favoriteMusic });
+      this.setState({
+        loading: false,
+      });
+    });
+    this.setState({ favorites: favoriteMusic });
+  }
+
   render() {
+    const { favorites, loading } = this.state;
+    console.log(favorites);
     return (
       <div data-testid="page-favorites" className="favorites-container">
         <Header />
+        {
+          loading
+            ? <Loading />
+            : favorites.map((e) => <MusicCard key={ e.trackId } trackSong={ e } />)
+        }
       </div>
     );
   }
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class NotFound extends Component {
   render() {
     return (
-      <div data-testid="page-not-found" className="page-not-found" />
+      <div data-testid="page-not-found" className="page-not-found">
+        ERRO 404 - Page Not Found!!!
+      </div>
     );
   }
 }
